f132857f37d3c4209fc23367f87089d419367776
SandrineFaucher <sandrinefaucher2606@gmail.com>
2023-06-20 16:46:40 +0200

    migrations, seeders et modèles en place

---
+++ app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // je charge automatiquement l'utilisateur à chaque fois que je récupère un message
+    protected $with = ['user'];
+
+    // nom de la fonction au singulier car 1 seul user en relation
+    // cardinalité 1,1
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //nom au pluriel car un message peut regrouper plusieurs commentaires
+    //cardinalité 0,n
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,32 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+// nom au pluriel car un user peut poster plusieurs quacks
+//cardinalité 0,n
+public function post()
+{
+    return $this->hasMany(Post::class);
+}
 
+// nom au pluriel car  un user peut poster plusieurs commentaires
+//cardinalité 0,n
+public function comments()
+{
+    return $this->hasMany(Comments::class);
+}
+
+//nom de la fonction au singulier car 1 seul rôle en relation
+// cardinalité 1,1
+public function role()
+{
+    return $this->belongsTo(Role::class);
+}
+
+public function isAdmin()
+{
+    //return $this->role == "admin";
+    return $this->role_id ==2;
+}
     /**
      * The attributes that are mass assignable.
      *
@@ -42,4 +67,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+
+
 }
